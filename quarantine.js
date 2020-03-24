98e9a03f689802bccb38f7efb30d8ca90dbc4893
Caroline <45307611+Carolinedanslesnuages@users.noreply.github.com>
2020-03-24 20:12:38 +0100

    Update quarantine.js

--- quarantine.js
+++ quarantine.js
@@ -1,8 +1,18 @@
+function myQuarantine(id) {
+ 
  var quarantine = new Date("Tue Mar 16 2020 12:00:00");
  var currentDate = new Date()
  var date_diff = currentDate.getDate() - quarantine.getDate();
  var time_diff = currentDate.getHours() - quarantine.getHours();
  var minutesDiff = currentDate.getMinutes() - quarantine.getMinutes();
- var secondsDiff = currentDate.getSeconds() - quarantine.getSeconds();
+ var seconde = currentDate.getSeconds() - quarantine.getSeconds();
  var phrase = 'depuis le début du confinement'
-document.getElementById("day").innerHTML = date_diff +' jour(s) ' + time_diff  + ' heures ' + minutesDiff + ' minutes ' + secondsDiff + ' secondes ' +  phrase
+document.getElementById("day").innerHTML = date_diff +' jour(s) ' + time_diff  + ' heures ' + minutesDiff + ' minutes ' + seconde + ' Secondes ' +  phrase
+
+  if(seconde<100){
+    seconde = seconde
+  }
+  setTimeout('myQuarantine("'+id+'");','100')
+  return true;
+}
+myQuarantine("day");
