a7beb9d6504d48c73c1ab5d8fddea3e567a7ee3d
user.name like "carolinedanslesnuages <user.email like "line.robillard@gmail.com>
2020-03-27 22:07:13 +0100

    :tada: Add tailwind css

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    'postcss-preset-env': {},
+    'postcss-nested-ancestors': {},
+    'postcss-nested': {},
+    'postcss-current-selector': {},
+    'postcss-apply': {},
+    'vue-cli-plugin-tailwind/purgecss': {
+      content: [
+        './src/**/*.html',
+        './src/**/*.vue',
+      ],
+    },
+  },
+}
