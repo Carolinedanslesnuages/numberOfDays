6120998a7ea4e28fdbfbb176bfd87343b9803dfd
user.name like "carolinedanslesnuages <user.email like "line.robillard@gmail.com>
2020-04-03 09:52:04 +0200

    fix calcul date and tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,5 @@ module.exports = {
       'active',
       'disabled',
     ],
-  },
-  plugins: {
-    'postcss-fontpath': { checkFiles: true, ie8Fix: true },
-    'tailwindcss': 'tailwind.config.js',
-    '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production',
-    'autoprefixer': {},
-  },
+  }
 }
